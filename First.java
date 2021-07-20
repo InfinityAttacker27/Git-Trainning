40204c312cf002893f94d8182d33d5b9ac276820
InfinityAttacker27 <81138276+InfinityAttacker27@users.noreply.github.com>
2021-07-20 17:05:47 +0000

    Update First.java
    Starting the Training

--- First.java
+++ First.java
@@ -9,5 +9,6 @@ public static void main(String args[])
 	System.out.println("Testing is successfully completed");
 	System.out.println("Please Start the lessions");
 	System.out.println("This is My New Commit");
+	System.out.println("Let's Start our Training");
 }
 }
