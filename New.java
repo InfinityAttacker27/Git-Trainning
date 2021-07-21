34a8ee4eb46f5daa925a099080f396d1eec27bc5
InfinityAttacker27 <vmathics27@gmail.com>
2021-07-21 08:50:09 +0530

    This is from Eclipse IDE

--- New.java
+++ New.java
@@ -1,7 +1,7 @@
-public class New
-{
-public static void main(String args[])
-{
-System.out.println("Hi to All");
-}
+public class New {
+	public static void main(String args[]) {
+		System.out.println("Hi to All");
+		System.out.println("This is from the Eclipse Git Working...");
+		sysou
+	}
 }
